01da416a606232e8b859b1eb0810fac625931e0c
Dan Barkwell <dbarkwell@pelism.com>
2018-11-11 11:54:01 -0500

    Add model

---
+++ src/Ocelot.ConfigEditor/Editor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ocelot.ConfigEditor.Editor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+ 
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
