a8601abe1e108e0e302e53638d89a3f752cbd7f3
Dan Barkwell <dbarkwell@pelism.com>
2018-12-29 10:23:28 -0500

    Rename and add additional methods

---
+++ src/Ocelot.ConfigEditor/ConfigurationService.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Ocelot.Configuration.File;
+using Ocelot.Configuration.Repository;
+using Ocelot.Configuration.Setter;
+using Ocelot.Responses;
+
+namespace Ocelot.ConfigEditor
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        private const string ReloadFileName = "configeditor.reload";
+        private readonly IFileConfigurationSetter _configSetter;
+        private readonly IFileConfigurationRepository _fileConfigRepo;
+
+        public ConfigurationService(IFileConfigurationSetter configSetter, IFileConfigurationRepository fileConfigRepo)
+        {
+            _configSetter = configSetter;
+            _fileConfigRepo = fileConfigRepo;
+        }
+
+        public bool IsReloadRequired()
+        {
+            return File.Exists(ReloadFileName);
+        }
+
+        public async Task ReloadConfig()
+        {
+            var config = await GetConfig();
+            await SetConfig(config.Data);
+            
+            RemoveReloadFlag();
+        }
+
+        public void AddReloadFlag()
+        {
+            using (var fileStream = File.Create(ReloadFileName))
+            {
+            }
+        }
+
+        public void RemoveReloadFlag()
+        {
+            if (File.Exists(ReloadFileName)) 
+                File.Delete(ReloadFileName);
+        }
+
+        public async Task<Response<FileConfiguration>> GetConfig()
+        {
+            return await _fileConfigRepo.Get();
+        }
+
+        public async Task SetConfig(FileConfiguration fileConfig)
+        {
+            await _configSetter.Set(fileConfig);
+        }
+    }
+}
---
+++ src/Ocelot.ConfigEditor/IConfigurationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using Ocelot.Configuration.File;
+using Ocelot.Responses;
+
+namespace Ocelot.ConfigEditor
+{
+    public interface IConfigurationService
+    {
+        bool IsReloadRequired();
+
+        void AddReloadFlag();
+        
+        void RemoveReloadFlag();
+        
+        Task ReloadConfig();
+
+        Task<Response<FileConfiguration>> GetConfig();
+
+        Task SetConfig(FileConfiguration fileConfig);
+    }
+}
